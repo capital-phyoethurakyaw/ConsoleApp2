61a9cbf22457afe39c4d269b70e670db2050d134
capital.phyoethurakyaw@gmail.com <capital.phyoethurakyaw@gmail.com>
2023-03-01 10:30:47 +0630

    ptk

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         { 
-            var b1 = "";
+            //pyk var b1 = "";
             var b2 = "";
         }
     }
